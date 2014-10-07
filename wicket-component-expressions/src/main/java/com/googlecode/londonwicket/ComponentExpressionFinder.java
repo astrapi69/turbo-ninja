7a2c894222af76ef3c92c8119caf93e64d4b800c
astrapi69 <asterios.raptis@gmx.net>
2014-10-07 13:01:44 +0200

    Util class for finding components from parent component.

---
+++ wicket-component-expressions/src/main/java/com/googlecode/londonwicket/ComponentExpressionFinder.java
@@ -0,0 +1,33 @@
+package com.googlecode.londonwicket;
+import java.util.List;
+
+import org.apache.wicket.Component;
+
+/**
+ * The Class ComponentFinder.
+ */
+public final class ComponentExpressionFinder {
+
+	/**
+	 * Finds all children from the given parent class. The result can be added
+	 * to a ajax target so the components can be automatically updated.
+	 * 
+	 * @param childComponent
+	 *            This should be a child component from the given parent class
+	 * @param parentClass
+	 *            The parent class
+	 * @param childClass
+	 *            The child class for the result
+	 * 
+	 * @return An array from the child components from the parameter childClass.
+	 **/
+	public static Component[] findAllChildrenFromParent(
+			Component childComponent, Class<? extends Component> parentClass,
+			Class<? extends Component> childClass) {
+		Component parent = childComponent.findParent(parentClass);
+		List<Component> children = ComponentExpression.findAllComponents(
+				parent, "**", childClass);
+		return children.toArray(new Component[children.size()]);
+	}
+	
+}
