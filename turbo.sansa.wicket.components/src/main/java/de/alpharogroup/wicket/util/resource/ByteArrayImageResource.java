7b68cba1be72bec6f57e5cc17bf9471853f22628
astrapi69 <asterios.raptis@gmx.net>
2014-10-11 16:16:28 +0200

    A byte array resource for entity Resources.

---
+++ turbo.sansa.wicket.components/src/main/java/de/alpharogroup/wicket/util/resource/ByteArrayImageResource.java
@@ -0,0 +1,16 @@
+package de.alpharogroup.wicket.util.resource;
+
+import org.apache.commons.lang.ArrayUtils;
+import org.jaulp.wicket.base.util.resource.DatabaseImageResource;
+
+import resource.system.model.Resources;
+
+public class ByteArrayImageResource extends DatabaseImageResource {
+
+	private static final long serialVersionUID = 1L;
+
+	public ByteArrayImageResource(Resources resources) {
+		super(resources.getContentType(), ArrayUtils.toPrimitive( resources.getContent() ));
+	}
+
+}
