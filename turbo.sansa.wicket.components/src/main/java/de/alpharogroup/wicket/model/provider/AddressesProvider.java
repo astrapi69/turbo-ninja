530440ad4385a79490af85cf60933b87657568ab
astrapi69 <asterios.raptis@gmx.net>
2014-10-23 20:08:06 +0200

    new data profider for entity Addresses.

---
+++ turbo.sansa.wicket.components/src/main/java/de/alpharogroup/wicket/model/provider/AddressesProvider.java
@@ -0,0 +1,32 @@
+package de.alpharogroup.wicket.model.provider;
+
+import java.util.List;
+
+import org.jaulp.wicket.data.provider.AbstractSortableDataProvider;
+
+import address.book.model.Addresses;
+
+/**
+ * The Class AddressesProvider.
+ * 
+ * @author Asterios Raptis
+ */
+public class AddressesProvider extends
+		AbstractSortableDataProvider<Addresses, String> {
+
+	/**
+	 * The serialVersionUID.
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Instantiates a new addresses provider.
+	 * 
+	 * @param data
+	 *            the data
+	 */
+	public AddressesProvider(List<Addresses> data) {
+		super(data);
+	}
+
+}
