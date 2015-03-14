916dfb6d0a84b26239f915810349305019555434
astrapi69 <asterios.raptis@gmx.net>
2015-03-14 13:13:48 +0100

    All methods have now a java doc.

--- wicket-component-expressions/src/main/java/de/alpharogroup/wicket/component/search/ComponentExpression.java
+++ wicket-component-expressions/src/main/java/de/alpharogroup/wicket/component/search/ComponentExpression.java
@@ -28,90 +28,204 @@ import java.util.regex.Pattern;
 import org.apache.wicket.Component;
 import org.apache.wicket.MarkupContainer;
 
-public class ComponentExpression {
+/**
+ * This class provides several methods for the search of wicket {@link org.apache.wicket.Component}
+ * s.
+ */
+public class ComponentExpression
+{
 
 	private static final String ANY_COMPONENT_MATCHER = "*";
 	private static final String ANY_COMPONENT_RECURSIVE_MATCHER = "**";
 
-	public static Component findComponent(Component parent, String expression) {
+	/**
+	 * Search for the first {@link org.apache.wicket.Component} with the given expression in the
+	 * given parent. Internally this method calls the
+	 * {@link ComponentExpression#findAllComponents(org.apache.wicket.Component, String)} that
+	 * returns a list and if this list is not empty the first element will be returned.
+	 * 
+	 * @param parent
+	 *            the parent that will be the start point to search.
+	 * @param expression
+	 *            the expression for search.
+	 * @return the first {@link org.apache.wicket.Component} with the given expression in the given
+	 *         parent or null if nothing is found.
+	 */
+	public static Component findComponent(Component parent, String expression)
+	{
 		List<Component> results = findAllComponents(parent, expression);
-		if (results.isEmpty()) {
+		if (results.isEmpty())
+		{
 			return null;
-		} else {
+		}
+		else
+		{
 			return results.get(0);
 		}
 	}
 
-	public static Component findComponent(Component parent, String expression, Class<? extends Component> typeRestriction) {
+	/**
+	 * Search for the first {@link org.apache.wicket.Component} with the given expression in the
+	 * given parent with the given class type restriction. Internally this method calls the
+	 * {@link ComponentExpression#findAllComponents(org.apache.wicket.Component, String, Class)}
+	 * that returns a list and if this list is not empty the first element will be returned.
+	 * 
+	 * @param parent
+	 *            the parent that will be the start point to search.
+	 * @param expression
+	 *            the expression for search.
+	 * @param typeRestriction
+	 *            the class type restriction for the search.
+	 * @return the first {@link org.apache.wicket.Component} with the given expression in the given
+	 *         parent or null if nothing is found.
+	 */
+	public static Component findComponent(Component parent, String expression,
+		Class<? extends Component> typeRestriction)
+	{
 		List<Component> results = findAllComponents(parent, expression, typeRestriction);
-		if (results.isEmpty()) {
+		if (results.isEmpty())
+		{
 			return null;
-		} else {
+		}
+		else
+		{
 			return results.get(0);
 		}
 	}
 
-	public static List<Component> findAllComponents(Component parent, String expression) {
+	/**
+	 * Search for all {@link org.apache.wicket.Component}s with the given expression in the given
+	 * parent with a {@link org.apache.wicket.Component} class type restriction.
+	 *
+	 * @param parent
+	 *            the parent that will be the start point to search.
+	 * @param expression
+	 *            the expression for search.
+	 * @return all found {@link org.apache.wicket.Component}s in a {@link java.util.List} with the
+	 *         given expression in the given parent with the given class type restriction or an
+	 *         empty {@link java.util.List} if nothing is found.
+	 */
+	public static List<Component> findAllComponents(Component parent, String expression)
+	{
 		return findAllComponents(parent, expression, Component.class);
 
 	}
 
-	public static List<Component> findAllComponents(Component parent, String expression, Class<? extends Component> typeRestriction) {
-		if (expression == null || expression.equals("")) {
+	/**
+	 * Search for all {@link org.apache.wicket.Component}s with the given expression in the given
+	 * parent with the given class type restriction.
+	 *
+	 * @param parent
+	 *            the parent that will be the start point to search.
+	 * @param expression
+	 *            the expression for search.
+	 * @param typeRestriction
+	 *            the class type restriction for the search.
+	 * @return all found {@link org.apache.wicket.Component}s in a {@link java.util.List} with the
+	 *         given expression in the given parent with the given class type restriction or an
+	 *         empty {@link java.util.List} if nothing is found.
+	 */
+	public static List<Component> findAllComponents(Component parent, String expression,
+		Class<? extends Component> typeRestriction)
+	{
+		if (expression == null || expression.equals(""))
+		{
 			return Collections.emptyList();
 		}
-		return findComponent(parent, new LinkedList<String>(Arrays.asList(expression.split(Character.toString(Component.PATH_SEPARATOR)))), typeRestriction);
+		return findComponent(
+			parent,
+			new LinkedList<String>(Arrays.asList(expression.split(Character
+				.toString(Component.PATH_SEPARATOR)))), typeRestriction);
 	}
 
-	private static List<Component> findComponent(Component parent, LinkedList<String> expressionListIn, Class<? extends Component> typeRestriction) {
+	/**
+	 * Search for all {@link org.apache.wicket.Component}s with the given list of expressions in the
+	 * given parent with the given class type restriction.
+	 *
+	 * @param parent
+	 *            the parent that will be the start point to search.
+	 * @param expressionListIn
+	 *            the list with the expressions for the search.
+	 * @param typeRestriction
+	 *            the class type restriction for the search.
+	 * @return all found {@link org.apache.wicket.Component}s in a {@link java.util.List} with the
+	 *         given expression in the given parent with the given class type restriction or an
+	 *         empty {@link java.util.List} if nothing is found.
+	 */
+	private static List<Component> findComponent(Component parent,
+		LinkedList<String> expressionListIn, Class<? extends Component> typeRestriction)
+	{
 
 		LinkedList<String> expressionList = new LinkedList<String>(expressionListIn);
 
-		if (expressionList.isEmpty()) {
-			if (typeRestriction.isAssignableFrom(parent.getClass())) {
+		if (expressionList.isEmpty())
+		{
+			if (typeRestriction.isAssignableFrom(parent.getClass()))
+			{
 				return Arrays.asList(parent);
-			} else {
+			}
+			else
+			{
 				return Collections.emptyList();
 			}
-		} else {
+		}
+		else
+		{
 			String rawFirst = expressionList.getFirst();
 			Collection<Condition> conditions = parseConditions(rawFirst);
 			final String first = rawFirst.replaceAll("\\s*\\[.*\\]\\s*", "");
 
-			if (!first.equals(ANY_COMPONENT_RECURSIVE_MATCHER)) {
+			if (!first.equals(ANY_COMPONENT_RECURSIVE_MATCHER))
+			{
 				expressionList.removeFirst();
 				List<Component> allMatches = getChild(parent, first, conditions);
-				if (allMatches.isEmpty()) {
+				if (allMatches.isEmpty())
+				{
 					return Collections.emptyList();
-				} else {
+				}
+				else
+				{
 					List<Component> finallyMatchedComponents = new ArrayList<Component>();
-					for (Component aMatch : allMatches) {
-						finallyMatchedComponents.addAll(findComponent(aMatch, expressionList, typeRestriction));
+					for (Component aMatch : allMatches)
+					{
+						finallyMatchedComponents.addAll(findComponent(aMatch, expressionList,
+							typeRestriction));
 					}
 					return finallyMatchedComponents;
 				}
-			} else if (expressionList.size() == 1) {
+			}
+			else if (expressionList.size() == 1)
+			{
 				List<Component> allMatches = new ArrayList<Component>();
-				if (parent instanceof MarkupContainer) {
-					for (Component aMatch : getAllChildren((MarkupContainer) parent)) {
+				if (parent instanceof MarkupContainer)
+				{
+					for (Component aMatch : getAllChildren((MarkupContainer)parent))
+					{
 						if (typeRestriction.isAssignableFrom(aMatch.getClass())
-								&& evaluateConditions(conditions, aMatch)) {
+							&& evaluateConditions(conditions, aMatch))
+						{
 							allMatches.add(aMatch);
 						}
 						allMatches.addAll(findComponent(aMatch, expressionList, typeRestriction));
 					}
 					return allMatches;
-				} else {
+				}
+				else
+				{
 					return Collections.emptyList();
 				}
-			} else {
+			}
+			else
+			{
 				List<Component> allMatches = new ArrayList<Component>();
 				LinkedList<String> fake = new LinkedList<String>();
 				fake.add(ANY_COMPONENT_RECURSIVE_MATCHER);
 				List<Component> allPotentialParents = findComponent(parent, fake, Component.class);
 				expressionList.removeFirst();
-				for (Component aParent : allPotentialParents) {
-					if(evaluateConditions(conditions, aParent)){
+				for (Component aParent : allPotentialParents)
+				{
+					if (evaluateConditions(conditions, aParent))
+					{
 						allMatches.addAll(findComponent(aParent, expressionList, typeRestriction));
 					}
 				}
@@ -120,78 +234,140 @@ public class ComponentExpression {
 		}
 	}
 
-	private static List<Component> getChild(Component parent, String expression, Collection<Condition> conditions) {
+	/**
+	 * Search for all child {@link org.apache.wicket.Component}s with the given expression and the
+	 * given parent with the given collection of {@link Condition}s as filter.
+	 * 
+	 * @param parent
+	 *            the parent that will be the start point to search.
+	 * @param expression
+	 *            the expression for search.
+	 * @param conditions
+	 *            a collection of {@link Condition}s to filter.
+	 * @return all found child {@link org.apache.wicket.Component}s in a {@link java.util.List}.
+	 */
+	private static List<Component> getChild(Component parent, String expression,
+		Collection<Condition> conditions)
+	{
 
-		if (parent instanceof MarkupContainer) {
-			MarkupContainer parentContainer = (MarkupContainer) parent;
-			if (expression.equals(ANY_COMPONENT_MATCHER)) {
+		if (parent instanceof MarkupContainer)
+		{
+			MarkupContainer parentContainer = (MarkupContainer)parent;
+			if (expression.equals(ANY_COMPONENT_MATCHER))
+			{
 				List<Component> allChildren = getAllChildren(parentContainer);
 				List<Component> allChildrenMatchingCondition = new ArrayList<Component>();
-				for (Component child : allChildren) {
-					if (child != null && evaluateConditions(conditions, child)) {
+				for (Component child : allChildren)
+				{
+					if (child != null && evaluateConditions(conditions, child))
+					{
 						allChildrenMatchingCondition.add(child);
 					}
 				}
 				return allChildrenMatchingCondition;
-			} else {
+			}
+			else
+			{
 
 				Component comp = parentContainer.get(expression);
 
-				if (comp == null || !evaluateConditions(conditions, comp)) {
+				if (comp == null || !evaluateConditions(conditions, comp))
+				{
 					return Collections.emptyList();
-				} else {
+				}
+				else
+				{
 					return Arrays.asList(comp);
 				}
 			}
-		} else {
+		}
+		else
+		{
 			return Collections.emptyList();
 		}
 	}
 
-	private static final boolean evaluateConditions(Collection<Condition> conditions, Component component) {
+	/**
+	 * Evaluates the given {@link org.apache.wicket.Component} with the given collection of
+	 * {@link Condition}s.
+	 * 
+	 * @param conditions
+	 *            a collection of {@link Condition}s to evaluate.
+	 * @param component
+	 *            the {@link org.apache.wicket.Component} that the evaluation will be executed.
+	 * @return true if the evaluation is successful otherwise false.
+	 */
+	private static final boolean evaluateConditions(Collection<Condition> conditions,
+		Component component)
+	{
 		boolean result = true;
-		for (Condition condition : conditions) {
-			switch (condition.getAttr()) {
-			case ENABLED: {
-				result = result && (component.isEnabledInHierarchy() == condition.getCondition());
-				break;
+		for (Condition condition : conditions)
+		{
+			switch (condition.getAttr())
+			{
+				case ENABLED : {
+					result = result
+						&& (component.isEnabledInHierarchy() == condition.getCondition());
+					break;
+				}
+				case VISIBLE : {
+					result = result
+						&& (component.isVisibleInHierarchy() == condition.getCondition());
+				}
 			}
-			case VISIBLE: {
-				result = result && (component.isVisibleInHierarchy() == condition.getCondition());
-			}
-			}
-			if (!result) {
+			if (!result)
+			{
 				break;
 			}
 		}
 		return result;
 	}
 
-	private static final Collection<Condition> parseConditions(String expression) {
+	/**
+	 * Parses the given expression to a collection of {@link Condition}s.
+	 * 
+	 * @param expression
+	 *            the expression to parse.
+	 * @return a collection of {@link Condition}s.
+	 */
+	private static final Collection<Condition> parseConditions(String expression)
+	{
 		List<Condition> parsedConditions = new ArrayList<Condition>();
 		Pattern pattern = Pattern.compile(".*\\[\\s*(.*?)\\s*\\]\\w*", Pattern.CASE_INSENSITIVE);
 		Matcher matcher = pattern.matcher(expression);
-		if (matcher.matches()) {
+		if (matcher.matches())
+		{
 			String conditionString = matcher.group(1);
 			String[] conditions = conditionString.split("\\s*(and|&)\\s*");
-			for (String aCondition : conditions) {
+			for (String aCondition : conditions)
+			{
 				Condition.Attr attr = null;
 				Boolean condition = null;
 				String[] parts = null;
-				if (aCondition.contains("=")) {
+				if (aCondition.contains("="))
+				{
 					parts = aCondition.split("\\s*=\\s*");
-					if (parts.length == 2) {
-						if (parts[0].toUpperCase().equals(Condition.Attr.VISIBLE.toString())) {
+					if (parts.length == 2)
+					{
+						if (parts[0].toUpperCase().equals(Condition.Attr.VISIBLE.toString()))
+						{
 							attr = Condition.Attr.VISIBLE;
-						} else if (parts[0].toUpperCase().equals(Condition.Attr.ENABLED.toString())) {
+						}
+						else if (parts[0].toUpperCase().equals(Condition.Attr.ENABLED.toString()))
+						{
 							attr = Condition.Attr.ENABLED;
 						}
-						if (parts[1].toUpperCase().equals(Boolean.TRUE.toString().toUpperCase())) {
+						if (parts[1].toUpperCase().equals(Boolean.TRUE.toString().toUpperCase()))
+						{
 							condition = Boolean.TRUE;
-						} else if (parts[1].toUpperCase().equals(Boolean.FALSE.toString().toUpperCase())) {
+						}
+						else if (parts[1].toUpperCase().equals(
+							Boolean.FALSE.toString().toUpperCase()))
+						{
 							condition = Boolean.FALSE;
 						}
-						if (attr != null && condition != null) {
+						if (attr != null && condition != null)
+						{
 							parsedConditions.add(new Condition(attr, condition));
 						}
 					}
@@ -202,37 +378,92 @@ public class ComponentExpression {
 		return parsedConditions;
 	}
 
-	private static final class Condition {
-		private enum Attr {
-			VISIBLE, ENABLED
+	/**
+	 * Simple pojo class that holds an attribute(visible or enabled) and a condition.
+	 */
+	private static final class Condition
+	{
+		/**
+		 * Simple enum that holds two states of an attribute.
+		 */
+		private enum Attr
+		{
+			/**
+			 * The visible state.
+			 */
+			VISIBLE,
+			/**
+			 * The enabled state.
+			 */
+			ENABLED;
 		}
 
+		/**
+		 * The state of the condition.
+		 */
 		private final Attr attr;
+		/**
+		 * The flag condition.
+		 */
 		private final boolean condition;
 
-		public Condition(Attr attr, boolean condition) {
+		/**
+		 * Default constructor.
+		 * 
+		 * @param attr
+		 *            The state of the condition.
+		 * @param condition
+		 *            The flag condition.
+		 */
+		public Condition(Attr attr, boolean condition)
+		{
 			this.attr = attr;
 			this.condition = condition;
 		}
 
-		public Attr getAttr() {
+		/**
+		 * Gets the state of the condition.
+		 * 
+		 * @return the state of the condition.
+		 */
+		public Attr getAttr()
+		{
 			return attr;
 		}
 
-		public boolean getCondition() {
+		/**
+		 * Gets the flag condition.
+		 * 
+		 * @return the flag condition.
+		 */
+		public boolean getCondition()
+		{
 			return condition;
 		}
 
+		/**
+		 * {@inheritDoc}
+		 */
 		@Override
-		public String toString() {
+		public String toString()
+		{
 			return String.format("Condition [attr=%s, condition=%s]", attr, condition);
 		}
 	}
 
-	private static List<Component> getAllChildren(MarkupContainer parent) {
+	/**
+	 * Search for all child {@link org.apache.wicket.Component}s from the given parent.
+	 * 
+	 * @param parent
+	 *            The parent
+	 * @return all child {@link org.apache.wicket.Component}s from the given parent.
+	 */
+	private static List<Component> getAllChildren(MarkupContainer parent)
+	{
 		List<Component> children = new ArrayList<Component>();
 		Iterator<? extends Component> iter = parent.iterator();
-		while (iter.hasNext()) {
+		while (iter.hasNext())
+		{
 			children.add(iter.next());
 		}
 		return children;
