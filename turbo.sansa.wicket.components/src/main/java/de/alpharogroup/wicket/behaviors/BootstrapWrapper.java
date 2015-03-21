9a6311c4968d0e7f4c371c3839d49136d6399805
astrapi69 <asterios.raptis@gmx.net>
2015-03-21 22:57:07 +0100

    import changes after refactoring.

--- turbo.sansa.wicket.components/src/main/java/de/alpharogroup/wicket/behaviors/BootstrapWrapper.java
+++ turbo.sansa.wicket.components/src/main/java/de/alpharogroup/wicket/behaviors/BootstrapWrapper.java
@@ -1,6 +1,6 @@
 package de.alpharogroup.wicket.behaviors;
 
-import org.jaulp.wicket.behaviors.AddJsQueryStatementsBehavior;
+import org.jaulp.wicket.behaviors.JqueryStatementsBehavior;
 import org.jaulp.wicket.behaviors.BuildableChainableStatement;
 import org.odlabs.wiquery.core.javascript.JsUtils;
 
@@ -16,31 +16,31 @@ public final class BootstrapWrapper {
 	}
 
 	/** The Constant ROW_ELEMENT. */
-	public static final AddJsQueryStatementsBehavior ROW_ELEMENT = new AddJsQueryStatementsBehavior()
+	public static final JqueryStatementsBehavior ROW_ELEMENT = new JqueryStatementsBehavior()
 	.add(new BuildableChainableStatement.Builder().label("wrap")
 			.args(JsUtils.quotes("<div class=\"row\"></div>"))
 			.build());
 
 	/** The Constant CONTROLS_ELEMENT. */
-	public static final AddJsQueryStatementsBehavior CONTROLS_ELEMENT = new AddJsQueryStatementsBehavior()
+	public static final JqueryStatementsBehavior CONTROLS_ELEMENT = new JqueryStatementsBehavior()
 			.add(new BuildableChainableStatement.Builder().label("wrap")
 					.args(JsUtils.quotes("<div class=\"controls\"></div>"))
 					.build());
 
 	/** The Constant INPUT_ELEMENT. */
-	public static final AddJsQueryStatementsBehavior INPUT_ELEMENT = new AddJsQueryStatementsBehavior()
+	public static final JqueryStatementsBehavior INPUT_ELEMENT = new JqueryStatementsBehavior()
 	.add(new BuildableChainableStatement.Builder().label("wrap")
 			.args(JsUtils.quotes("<div class=\"input-group\"></div>"))
 			.build());
 
 	/** The Constant INPUT_GROUP_BUTTON_ELEMENT. */
-	public static final AddJsQueryStatementsBehavior INPUT_GROUP_BUTTON_ELEMENT = new AddJsQueryStatementsBehavior()
+	public static final JqueryStatementsBehavior INPUT_GROUP_BUTTON_ELEMENT = new JqueryStatementsBehavior()
 	.add(new BuildableChainableStatement.Builder().label("wrap")
 			.args(JsUtils.quotes("<span class=\"input-group-btn\"></span>"))
 			.build());
 
 	/** The Constant CONTROL_GROUP_ELEMENT. */
-	public static final AddJsQueryStatementsBehavior CONTROL_GROUP_ELEMENT = new AddJsQueryStatementsBehavior()
+	public static final JqueryStatementsBehavior CONTROL_GROUP_ELEMENT = new JqueryStatementsBehavior()
 			.add(new BuildableChainableStatement.Builder().label("wrap")
 					.args(JsUtils.quotes("<div class=\"control-group\"></div>"))
 					.build());
