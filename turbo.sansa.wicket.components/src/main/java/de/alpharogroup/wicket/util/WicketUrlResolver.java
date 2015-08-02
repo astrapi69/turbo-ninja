07a608018b846683c4864099ce19cac5a423f9d7
astrapi69 <asterios.raptis@gmx.net>
2015-08-02 13:51:12 +0200

    Renamed class with suffix Utils with suffix Extensions

--- turbo.sansa.wicket.components/src/main/java/de/alpharogroup/wicket/util/WicketUrlResolver.java
+++ turbo.sansa.wicket.components/src/main/java/de/alpharogroup/wicket/util/WicketUrlResolver.java
@@ -2,13 +2,34 @@ package de.alpharogroup.wicket.util;
 
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
+
 import de.alpharogroup.wicket.base.pageparameters.ParameterKeys;
 import de.alpharogroup.wicket.base.util.url.WicketUrlExtensions;
 
 /**
  * The Class WicketUrlResolver.
  */
-public class WicketUrlResolver {
+public class WicketUrlResolver
+{
+
+	/**
+	 * Gets the url as String object for the forgotten password page.
+	 * 
+	 * @param <C>
+	 *            the generic type
+	 * @param username
+	 *            the username
+	 * @param generatedCode
+	 *            the generated code
+	 * @param resetPasswordPage
+	 *            the reset password page
+	 * @return the url for forgotten password
+	 */
+	public static <C extends WebPage> String getUrlForForgottenPassword(final String username,
+		final String generatedCode, final Class<C> resetPasswordPage)
+	{
+		return getUrlForForgottenPassword(username, generatedCode, resetPasswordPage, false);
+	}
 
 	/**
 	 * Gets the url as String object for the forgotten password page.
@@ -25,32 +46,13 @@ public class WicketUrlResolver {
 	 *            the with server port
 	 * @return the url for forgotten password
 	 */
-	public static <C extends WebPage> String getUrlForForgottenPassword(
-			String username, final String generatedCode,
-			Class<C> resetPasswordPage, boolean withServerPort) {
-		PageParameters parameters = new PageParameters();
+	public static <C extends WebPage> String getUrlForForgottenPassword(final String username,
+		final String generatedCode, final Class<C> resetPasswordPage, final boolean withServerPort)
+	{
+		final PageParameters parameters = new PageParameters();
 		parameters.add(ParameterKeys.USERNAME, username);
 		parameters.add(ParameterKeys.CONFIRMATION_CODE, generatedCode);
 		return WicketUrlExtensions.absoluteUrlFor(resetPasswordPage, parameters, withServerPort);
 	}
-	
-	/**
-	 * Gets the url as String object for the forgotten password page.
-	 * 
-	 * @param <C>
-	 *            the generic type
-	 * @param username
-	 *            the username
-	 * @param generatedCode
-	 *            the generated code
-	 * @param resetPasswordPage
-	 *            the reset password page
-	 * @return the url for forgotten password
-	 */
-	public static <C extends WebPage> String getUrlForForgottenPassword(
-			String username, final String generatedCode, 
-			Class<C> resetPasswordPage) {
-		return getUrlForForgottenPassword(username, generatedCode, resetPasswordPage, false);
-	}
-	
+
 }
