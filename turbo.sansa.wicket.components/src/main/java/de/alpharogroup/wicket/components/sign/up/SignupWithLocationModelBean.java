050fb8d5a57bf0460304628e711375a92adfead2
astrapi69 <asterios.raptis@gmx.net>
2015-12-29 09:41:59 +0100

    rename of sign up model

--- turbo.sansa.wicket.components/src/main/java/de/alpharogroup/wicket/components/sign/up/SignupWithLocationModelBean.java
+++ turbo.sansa.wicket.components/src/main/java/de/alpharogroup/wicket/components/sign/up/SignupWithLocationModelBean.java
@@ -1,8 +1,11 @@
 package de.alpharogroup.wicket.components.sign.up;
 
+import java.util.List;
+import java.util.Map;
+
 import de.alpharogroup.address.book.application.model.LocationModel;
-import de.alpharogroup.address.book.entities.Addresses;
 import de.alpharogroup.auth.models.BaseUsernameSignUpModel;
+import de.alpharogroup.wicket.model.dropdownchoices.StringTwoDropDownChoicesModel;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -15,18 +18,24 @@ import lombok.ToString;
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
-public class SignUpModel extends BaseUsernameSignUpModel implements LocationModel<Addresses>
+public class SignupWithLocationModelBean<T> extends BaseUsernameSignUpModel implements LocationModel<T>
 {
 
 	private static final long serialVersionUID = 1L;
 	@Getter
 	@Setter
-	private Addresses address;
+	private T address;
 	@Getter
 	@Setter
 	private String location;
 	@Getter
 	@Setter
 	private String selectedCountryName;
+	@Getter
+	@Setter
+	private Map<String, List<String>> countriesToZipcodes;
+	@Getter
+	@Setter
+	private StringTwoDropDownChoicesModel dropdownChoicesModel;
 
 }
